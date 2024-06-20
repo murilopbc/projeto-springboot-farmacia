698d30c6890046cffd8e60264e08315b145c9c84
murilopbc <murilopoltronieri3@gmail.com>
2024-06-20 09:53:56 -0300

    organizei controller

--- src/main/java/com/remedios/murilo/curso/controllers/AuthenticationController.java
+++ src/main/java/com/remedios/murilo/curso/controllers/AuthenticationController.java
@@ -2,12 +2,10 @@ package com.remedios.murilo.curso.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/auth")
